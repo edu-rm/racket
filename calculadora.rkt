11d1f6e581645101a185190aff8769c6a944bca1
Eduardo Rampon Meireles <37454606+edu-rm@users.noreply.github.com>
2022-03-31 10:50:45 -0300

    calculadora incrementada

--- calculadora.rkt
+++ calculadora.rkt
@@ -20,10 +20,41 @@
   (- a b)
   )
 
+(
+ define (dobro a)
+  (multiplicar a 2)
+  )
+(
+ define (triplo a)
+  (multiplicar a 3)
+  )
 
-(somar 2 2)
-(subtrair 5 3)
-(dividir 4 2)
-(multiplicar 3 4)
+(
+ define (metade a)
+  (dividir a 2)
+  )
 
-(dividir (multiplicar (somar 2 2)(somar 3 3) ) 8)
+(
+ define (quadrado a)
+  (multiplicar a a)
+  )
+
+(
+ define (cubo a)
+  (multiplicar (multiplicar a a) a)
+  )
+
+
+
+;(somar 2 2)
+;(subtrair 5 3)
+;(dividir 4 2)
+;(multiplicar 3 4)
+(dobro 3)
+(triplo 3)
+(metade 4)
+(quadrado 4)
+(cubo 4)
+
+
+;(dividir (multiplicar (somar 2 2)(somar 3 3) ) 8)
